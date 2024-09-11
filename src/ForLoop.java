2a22c54d3381cca27b7d8efb97138c5850b4d1f5
Ikhsan Lintang Prakasa <ikhsanlintang06@gmail.com>
2024-09-11 19:56:10 +0700

    add: perulangan

---
+++ src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        var counter = 10;
+        for (int i = 0; i <= counter; i++) {
+            System.out.println(i);
+        }
+    }
+}
